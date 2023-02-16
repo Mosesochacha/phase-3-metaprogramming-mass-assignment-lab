4ef64ee39d4c4f521ba4e5cb0a742d2eb8af74a7
mosesochacha <ochachamoses443@@gmail.com>
2023-02-16 18:33:02 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,34 @@
-class Person
-  def initialize(attributes)
-    attributes.map do |key, value|
-      self.class.attr_accessor(key)
+# class Person
+#   def initialize(attributes)
+#     attributes.map do |key, value|
+#       self.class.attr_accessor(key)
 
-      self.send("#{key}=", value)
-    end
-  end
+#       self.send("#{key}=", value)
+#     end
+#   end
+# end
+
+
+
+      or 
+class Person
+  attr_accessor :name,
+                :birthday,
+                :hair_color,
+                :eye_color,
+                :height,
+                :weight,
+                :handed,
+                :complexion,
+                :t_shirt_size,
+                :wrist_size,
+                :glove_size,
+                :pant_length,
+                :pant_width
+
+    def initialize(attributes)
+      attributes.each do |key , value|
+        self.send("#{key}=", value)
+        end
+      end
 end
